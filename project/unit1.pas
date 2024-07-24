33865a33fec1e673e2f78c58e19a1756a22df416
pedrofernandescyber <hello@pedrof.com>
2024-07-24 19:13:44 +0100

    Unlock BitLocker and more buttons
    *Application window in now centered: in the form: poDesigned >> poDesktopCenter
    *NEW: Ability to unlock the C: drive, after asking the user for the recovery key
    *NEW: Additional buttons to open the Command Prompt (CMD), the Notepad and the Task Manager - All of them also work on WinPE

--- project/unit1.pas
+++ project/unit1.pas
@@ -5,7 +5,8 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Windows;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  Windows, Process;
 type
 
   { TForm1 }
@@ -15,6 +16,12 @@ type
     Button2: TButton;
     Button3: TButton;
     Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
@@ -22,6 +29,10 @@ type
     procedure Button2Click(Sender: TObject);
     procedure Button3Click(Sender: TObject);
     procedure Button4Click(Sender: TObject);
+    procedure Button5Click(Sender: TObject);
+    procedure Button6Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
     procedure Label3Click(Sender: TObject);
   private
 
@@ -69,6 +80,91 @@ end;
 procedure TForm1.Button4Click(Sender: TObject);
 begin
   ShellExecute(0, 'open', 'https://www.pedrof.com', nil, nil, SW_SHOWNORMAL);
+end;
+
+procedure TForm1.Button5Click(Sender: TObject);
+var
+  AProcess: TProcess;
+begin
+  AProcess := TProcess.Create(nil);
+  try
+    AProcess.Executable := 'cmd.exe';
+    AProcess.Options := AProcess.Options;
+    AProcess.Execute;
+  finally
+    AProcess.Free;
+  end;
+end;
+
+procedure TForm1.Button6Click(Sender: TObject);
+var
+  AProcess: TProcess;
+begin
+  AProcess := TProcess.Create(nil);
+  try
+    AProcess.Executable := 'notepad.exe';
+    AProcess.Options := AProcess.Options;
+    AProcess.Execute;
+  finally
+    AProcess.Free;
+  end;
+end;
+
+procedure TForm1.Button7Click(Sender: TObject);
+var
+  Process: TProcess;
+  Command: TStringList;
+  BatchFileName: string;
+begin
+  Process := TProcess.Create(nil);
+  Command := TStringList.Create;
+  try
+    // Create the batch file content
+    Command.Add('@echo off');
+    Command.Add('manage-bde -protectors C: -get -Type RecoveryPassword');
+    Command.Add('echo.');
+    Command.Add('echo Enter recovery key for the C Drive - Works both with and without the hyphens');
+    Command.Add('set /p recovery_key="Key: "');
+    Command.Add('manage-bde -unlock C: -recoverypassword %recovery_key%');
+    Command.Add('echo.');
+    Command.Add('echo Press any key or close this window to exit');
+    Command.Add('pause');
+
+    // Save the batch file to %TEMP% directory
+    BatchFileName := SysUtils.GetEnvironmentVariable('TEMP') + PathDelim + 'fixer_unlock_drive.bat';
+    Command.SaveToFile(BatchFileName);
+
+    // Configure the process to execute cmd and run the batch file
+    Process.Executable := 'cmd.exe';
+    Process.Parameters.Add('/c');
+    Process.Parameters.Add(BatchFileName);
+    Process.Options := Process.Options + [poWaitOnExit];
+
+    // Run the process
+    Process.Execute;
+
+    // Delete the batch file after execution
+    DeleteFile(PChar(BatchFileName));
+
+  finally
+    Process.Free;
+    Command.Free;
+  end;
+
+end;
+
+procedure TForm1.Button8Click(Sender: TObject);
+var
+  AProcess: TProcess;
+begin
+  AProcess := TProcess.Create(nil);
+  try
+    AProcess.Executable := 'taskmgr.exe';
+    AProcess.Options := AProcess.Options;
+    AProcess.Execute;
+  finally
+    AProcess.Free;
+  end;
 end;
 
 procedure TForm1.Label3Click(Sender: TObject);
